ae52c402253f02e6a0ccb43a5cda531792a66168
Pouria Mellati <pouria@pouria.me>
2016-11-28 14:39:34 +1100

    Add passing test: ThriftFileToCompilationUnitSpec should honor the structNameToObjectAndTableName parameter.

--- src/test/scala/sqlpt/thriftplugin/ThriftFileToCompilationUnitSpec.scala
+++ src/test/scala/sqlpt/thriftplugin/ThriftFileToCompilationUnitSpec.scala
@@ -27,8 +27,6 @@ class ThriftFileToCompilationUnitSpec extends Specification with Helpers {
         |
       """.stripMargin)
 
-      val fileToCu = ThriftFileToCompilationUnit(structToCc, {case x => ObjectAndTableName(x, x)})
-
       fileToCu(thriftDoc) must beSuccessfulTry.withValue {generatedCu: Tree =>
         generatedCu must beSameScalaCodeAs(
           """
@@ -73,20 +71,103 @@ class ThriftFileToCompilationUnitSpec extends Specification with Helpers {
         |
       """.stripMargin)
 
-      val fileToCu = ThriftFileToCompilationUnit(structToCc, {case x => ObjectAndTableName(x, x)})
-
       fileToCu(thriftDoc) must beFailedTry[CompilationUnit]
         .withThrowable[RuntimeException]("Thrift type .* is not supported as a SQLpt column type.")
     }
 
-    "honor the structNameToObjectAndTableName parameter" in pending
+    "honor the structNameToObjectAndTableName parameter" in {
+      val thriftDoc = parseThriftDoc("""
+        |#@namespace scala myorg.mypackage
+        |
+        |struct Car {
+        |     1: required string car_id;
+        |}
+        |
+        |struct CarManufacturer {
+        |  1: required string name;
+        |}
+        |
+      """.stripMargin)
+
+      fileToCu(thriftDoc).get must beSameScalaCodeAs("""
+        |package myorg.mypackage
+        |
+        |import sqlpt.api._
+        |
+        |object Car extends TableDef {
+        |  override def name = "Car"
+        |
+        |  case class Columns(car_id: Column[Str] = "car_id")
+        |  override def cols = Columns()
+        |}
+        |
+        |object CarManufacturer extends TableDef {
+        |  override def name = "CarManufacturer"
+        |
+        |  case class Columns(name: Column[Str] = "name")
+        |  override def cols = Columns()
+        |}
+      """.stripMargin)
+
+      val fileToCuWithNameTranslations = ThriftFileToCompilationUnit(structToCc, {
+        case "Car"             => ObjectAndTableName(objectName = "Cars", tableName = "cars")
+        case "CarManufacturer" => ObjectAndTableName(objectName = "CarManufacturers", tableName = "car_manu")
+      })
+
+      fileToCuWithNameTranslations(thriftDoc).get must beSameScalaCodeAs("""
+        |package myorg.mypackage
+        |
+        |import sqlpt.api._
+        |
+        |object Cars extends TableDef {
+        |  override def name = "cars"
+        |
+        |  case class Columns(car_id: Column[Str] = "car_id")
+        |  override def cols = Columns()
+        |}
+        |
+        |object CarManufacturers extends TableDef {
+        |  override def name = "car_manu"
+        |
+        |  case class Columns(name: Column[Str] = "name")
+        |  override def cols = Columns()
+        |}
+      """.stripMargin)
+
+      val fileToCuWithNameTranslationsAndAnOmission = ThriftFileToCompilationUnit(structToCc, {
+        case "Car" => ObjectAndTableName(objectName = "Cars", tableName = "cars")
+      })
+
+      fileToCuWithNameTranslationsAndAnOmission(thriftDoc).get must beSameScalaCodeAs("""
+        |package myorg.mypackage
+        |
+        |import sqlpt.api._
+        |
+        |object Cars extends TableDef {
+        |  override def name = "cars"
+        |
+        |  case class Columns(car_id: Column[Str] = "car_id")
+        |  override def cols = Columns()
+        |}
+      """.stripMargin)
+
+      val fileToCuWithEverythingOmitted = ThriftFileToCompilationUnit(structToCc, Map.empty)
+
+      fileToCuWithEverythingOmitted(thriftDoc).get must beSameScalaCodeAs("""
+        |package myorg.mypackage
+        |
+        |import sqlpt.api._
+      """.stripMargin)
+    }
 
     "not fail if the thrift document is empty" in pending
 
     "not fail if there is no namespace declaration in the thrift document" in pending
   }
 
-  private val structToCc = ThriftStructToColumnsCaseClass(identity, identity)
+  private lazy val structToCc = ThriftStructToColumnsCaseClass(identity, identity)
+
+  private lazy val fileToCu = ThriftFileToCompilationUnit(structToCc, {case x => ObjectAndTableName(x, x)})
 
   private def parseThriftDoc(str: String): thrift.Document =
     thriftParser.document(toReader(str)).get
